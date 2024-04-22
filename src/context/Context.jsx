2769642b81c1c876bf20b07d6bbef13f052264b2
susthir <susthir19march@gmail.com>
2024-04-23 01:39:13 +0530

    gemini

---
+++ src/context/Context.jsx
@@ -0,0 +1,14 @@
+// const { createContext } = require("react");
+
+// export const Context =createContext();
+// const ContextProvider = (props) =>{
+//     const ContextValue ={
+
+//     }
+//     return(
+//         <Context.Provider value={ContextValue}>
+//             {props.children}
+//         </Context.Provider>
+//     )
+// }
+// export default ContextProvider
